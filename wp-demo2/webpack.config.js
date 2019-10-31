22aaab8581713341576fa6ea16419522507a53ce
decadehew <yong_323@hotmail.com>
2019-10-31 11:28:41 +0800

    解析css + postcss

--- wp-demo2/webpack.config.js
+++ wp-demo2/webpack.config.js
@@ -15,8 +15,36 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /.js$/,
+        test: /\.js$/,
+        exclude: /node_modules/,
         use: 'babel-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [ 'style-loader', 'css-loader' ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          // creates style nodes from JS strings
+          'style-loader',
+          // translates CSS into CommonJS
+          'css-loader',
+          // compiles Less to CSS
+          'less-loader',
+        ],
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          'postcss-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
       }
     ]
   }
